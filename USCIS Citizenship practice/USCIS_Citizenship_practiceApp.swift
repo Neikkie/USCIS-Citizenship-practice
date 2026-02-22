2ab7f64f63259e7ecd7ce36c5b5848bc1ff10005
Neikkie <neikkie21@outlook.com>
2026-02-22 12:11:38 -0500

    Initial Commit

---
+++ USCIS Citizenship practice/USCIS_Citizenship_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  USCIS_Citizenship_practiceApp.swift
+//  USCIS Citizenship practice
+//
+//  Created by Shanique Beckford on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct USCIS_Citizenship_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
